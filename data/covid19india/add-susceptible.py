b42812021e9ddd5c35932b73dfb962c432337c44
Jerome White <jerome@thewex.net>
2020-05-12 08:00:43 +0000

    Assume specified population is correct and use fraction

--- data/covid19india/add-susceptible.py
+++ data/covid19india/add-susceptible.py
@@ -6,13 +6,17 @@ import pandas as pd
 
 arguments = ArgumentParser()
 arguments.add_argument('--population', type=int)
+arguments.add_argument('--fraction', type=float, default=0.8)
 arguments.add_argument('--with-variance', action='store_true')
 args = arguments.parse_args()
 
 df = pd.read_csv(sys.stdin, index_col='date', parse_dates=True)
 
 f = np.random.poisson if args.with_variance else np.repeat
-values = f(args.population, len(df))
-susceptible = pd.Series(values, name='susceptible', index=df.index)
+values = f(args.population, len(df)) * np.clip(args.fraction, 0, 1)
+susceptible = (pd
+               .Series(values, name='susceptible', index=df.index)
+               .round())
+
 df = pd.concat((susceptible, df), axis='columns')
 df.to_csv(sys.stdout)
