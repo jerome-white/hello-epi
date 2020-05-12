06654cdf92f35578fd40927cb076ed8ccd14f5ac
Jerome White <jerome@thewex.net>
2020-05-12 08:03:04 +0000

    Support for user specified parameters

--- data/covid19india/get.sh
+++ data/covid19india/get.sh
@@ -2,8 +2,25 @@
 
 url="https://api.covid19india.org/csv/latest/state_wise_daily.csv"
 state=mh
-pop=10000000
+population=10000000
 
+while getopts "p:s:h" OPTION; do
+    case $OPTION in
+	p) population=$OPTARG ;;
+	s) state=`tr [:upper:] [:lower:] <<< $OPTARG` ;;
+        h)
+	    cat <<EOF
+Usage: $0 [options]
+ -p Population (default $population)
+ -s Two-letter abbreviation of state (default $state)
+EOF
+            exit
+            ;;
+        *) exit 1 ;;
+    esac
+done
+
+here=$(dirname $(realpath $0))
 wget --quiet --output-document=- $url | \
     python $here/get.py | \
     python $here/extract.py --state $state | \
