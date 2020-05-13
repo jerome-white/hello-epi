31c9d3c524059ede47e30b29b55ecd5cbeb1db6a
Jerome White <jerome@thewex.net>
2020-05-13 19:22:52 +0000

    Plotting susceptibles is optional

--- fitting/myviz.py
+++ fitting/myviz.py
@@ -1,4 +1,6 @@
 import sys
+import itertools as it
+import collections as cl
 from pathlib import Path
 from argparse import ArgumentParser
 
@@ -8,23 +10,29 @@ import matplotlib.pyplot as plt
 
 arguments = ArgumentParser()
 arguments.add_argument('--output', type=Path)
+arguments.add_argument('--with-susceptible', action='store_true')
 args = arguments.parse_args()
 
 index = 'index'
-kwargs = {
-    'x': 'Day',
-    'y': 'Population',
-    'hue': 'Compartment',
-}
+compartments = cl.deque([
+    'infected',
+    'recovered',
+    'deceased',
+])
+if args.with_susceptible:
+    compartments.appendleft('susceptible')
+ctitle = 'Compartment'
 
-df = pd.read_csv(sys.stdin)
-compartments = list(filter(lambda x: x != index, df.columns))
-df = (df
-      .melt(id_vars=[index],
-            value_vars=compartments,
-            var_name=kwargs['hue'],
-            value_name=kwargs['y'])
-      .rename(columns={index: kwargs['x']}))
+usecols = list(it.chain(compartments, [index]))
+df = pd.read_csv(sys.stdin, usecols=usecols)
+compartments = list(it.filterfalse(lambda x: x == index, df.columns))
+df = df.melt(id_vars=[index], value_vars=compartments, var_name=ctitle)
 
-sns.lineplot(data=df, **kwargs)
+sns.lineplot(x=index,
+             y='value',
+             hue=ctitle,
+             data=df)
+plt.grid(which='both')
+plt.xlabel('Day')
+plt.ylabel('Population')
 plt.savefig(args.output)
