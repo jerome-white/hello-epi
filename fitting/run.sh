454068baa787fc05e5f82b91bc010c8bfca7c01c
Jerome White <jerome@thewex.net>
2020-05-17 13:15:18 +0000

    Pipeline broken into parts
    PyMC3 has intermediary output that that confuses the pipe; best to
    make it its own job.

--- fitting/run.sh
+++ fitting/run.sh
@@ -14,8 +14,12 @@ train=`mktemp`; head --lines=-$tr_days $raw > $train
 
 outlook=`expr $(wc --lines $raw | cut --delimiter=' ' --fields=1) + $te_days`
 
-python estimate.py --population $pop < $train | \
-    python project.py --population $pop --initial $init --outlook $outlook | \
+params=`mktemp`
+python estimate.py --population $pop < $train > $params
+python project.py \
+       --population $pop \
+       --initial $init \
+       --outlook $outlook < $params | \
     python visualize.py --output fit.png
 
-rm $raw $init $train
+rm $raw $init $train $params
