450592e94218d34f1619da896892ba6ecdc13ac7
Jerome White <jerome@thewex.net>
2020-05-25 19:51:06 +0530

    Separate testing and training data
    Make a scatter plot from the raw data, separating testing and training
    using hue.

--- fitting/visualize.py
+++ fitting/visualize.py
@@ -1,25 +1,49 @@
 import sys
 import math
+import functools as ft
 import itertools as it
 import collections as cl
 from pathlib import Path
 from argparse import ArgumentParser
 
+import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from scipy import constants
 
-from util import Logger
+from util import Logger, dsplit
+
+def labeller(x, testing):
+    days = pd.Timedelta(testing, unit='D')
+    pivot = x.index.max() - days
+    return np.where(x.index > pivot, 'test', 'train')
+
+def relative(x):
+    diff = x.index - x.index.min()
+    return (diff
+            .to_series()
+            .apply(lambda y: y.total_seconds() / constants.day)
+            .to_numpy())
 
 arguments = ArgumentParser()
 arguments.add_argument('--output', type=Path)
 arguments.add_argument('--ground-truth', type=Path)
 arguments.add_argument('--project', type=int)
+arguments.add_argument('--testing-days', type=int)
 arguments.add_argument('--with-susceptible', action='store_true')
 args = arguments.parse_args()
 
-gt = pd.read_csv(args.ground_truth, index_col='date', parse_dates=['date'])
+#
+#
+#
+index = 'date'
+gt = (pd
+      .read_csv(args.ground_truth, parse_dates=[index])
+      .melt(id_vars=[index])
+      .set_index(index)
+      .assign(split=ft.partial(labeller, testing=args.testing_days),
+              days=relative))
 
 #
 #
@@ -46,23 +70,35 @@ pr = pr.melt(id_vars=[index], value_vars=compartments, var_name=by)
 #
 #
 (_, axes) = plt.subplots(nrows=len(compartments), sharex=True) #, sharey=True)
-palette = sns.color_palette()
 for (ax, (c, g)) in zip(axes, pr.groupby(by, sort=False)):
     Logger.info(c)
-
-    ax.plot(gt[c].to_numpy(),
-            color=palette[1],
-            marker='o',
-            markeredgewidth=1.5,
-            markeredgecolor='white')
 
     sns.lineplot(x=index,
                  y='value',
                  hue=by,
                  data=g,
+                 legend=False,
                  ax=ax)
 
-    ax.get_legend().remove()
+    view = gt.query('variable == "{}"'.format(c))
+    palette = sns.color_palette(palette='Set2',
+                                n_colors=len(view['split'].unique()))
+    sns.scatterplot(x='days',
+                    y='value',
+                    hue='split',
+                    data=view,
+                    legend=False,
+                    palette=palette,
+                    ax=ax)
+
+    # for (i, (k, v)) in enumerate(split._asdict().items(), 1):
+    #     ax.plot(v[c].to_numpy(),
+    #             color=palette[i],
+    #             marker='o',
+    #             markeredgewidth=1.5,
+    #             markeredgecolor='white')
+
+    # ax.get_legend().remove()
     ax.grid(which='both')
     ax.set_xlabel('Day')
     ax.set_ylabel(c.title())
