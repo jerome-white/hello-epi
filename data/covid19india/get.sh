084c582c67e976f93e69ec761842749338ad915e
Jerome White <jerome@thewex.net>
2020-05-26 13:37:56 +0530

    Update to new workflow
    1. Get data from the JSON
    2. Un-aggregate it

--- data/covid19india/get.sh
+++ data/covid19india/get.sh
@@ -1,26 +1,5 @@
 #!/bin/bash
 
-url="https://api.covid19india.org/csv/latest/state_wise_daily.csv"
-state=mh
-population=10000000
-
-while getopts "p:s:h" OPTION; do
-    case $OPTION in
-	p) population=$OPTARG ;;
-	s) state=`tr [:upper:] [:lower:] <<< $OPTARG` ;;
-        h)
-	    cat <<EOF
-Usage: $0 [options]
- -p Population (default $population)
- -s Two-letter abbreviation of state (default $state)
-EOF
-            exit
-            ;;
-        *) exit 1 ;;
-    esac
-done
-
 here=$(dirname $(realpath $0))
-wget --quiet --output-document=- $url | \
-    python $here/get.py | \
-    python $here/extract.py --state $state
+python $here/state-wise-daily.py | \
+    python $here/unaggregate.py
