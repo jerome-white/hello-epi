fa742f19f9f2c5e8508707035d6ddadd08c1402b
Jerome White <jerome@thewex.net>
2020-05-26 13:27:05 +0530

    Unaggregate the state-wise-daily data

---
+++ data/covid19india/unaggregate.py
@@ -0,0 +1,32 @@
+import sys
+import collections as cl
+from argparse import ArgumentParser
+from multiprocessing import Pool
+
+import pandas as pd
+
+Group = cl.namedtuple('Group', ['state', 'district'])
+
+def func(args):
+    (group, df) = args
+
+    return (df
+            .filter(items=['infected', 'recovered', 'deceased'])
+            .diff()
+            .dropna()
+            .clip(lower=0)
+            .astype(int)
+            .assign(**group._asdict()))
+
+def each(fp):
+    df = pd.read_csv(sys.stdin, index_col='date', parse_dates=['date'])
+    for (i, g) in df.groupby(list(Group._fields), sort=False):
+        yield (Group(*i), g)
+
+arguments = ArgumentParser()
+arguments.add_argument('--workers', type=int)
+args = arguments.parse_args()
+
+with Pool(args.workers) as pool:
+    records = pool.imap_unordered(func, each(sys.stdin))
+    pd.concat(records).to_csv(sys.stdout)
