76dce433b691f4dcd7290d40c8bf556922e48e7f
Jerome White <jerome@thewex.net>
2020-05-28 20:53:39 +0530

    Provide the option for disaggregation

--- data/covid19india/clean.py
+++ data/covid19india/clean.py
@@ -8,29 +8,33 @@ import pandas as pd
 Group = cl.namedtuple('Group', ['state', 'district'])
 
 def func(args):
-    (group, df) = args
+    (dis, group, df) = args
 
-    return (df
+    view = (df
             .filter(items=['infected', 'recovered', 'deceased'])
             .resample('D')
-            .ffill()
-            # .diff()
-            # .fillna(0)
+            .ffill())
+    if dis:
+        view = view.diff().fillna(0)
+    view = (view
             .clip(lower=0)
             .astype(int)
             .assign(**group._asdict()))
 
-def each(fp):
+    return view
+
+def each(fp, args):
     index = 'date'
     df = pd.read_csv(sys.stdin, index_col=index, parse_dates=[index])
 
     for (i, g) in df.groupby(list(Group._fields), sort=False):
-        yield (Group(*i), g)
+        yield (args.disaggregate, Group(*i), g)
 
 arguments = ArgumentParser()
 arguments.add_argument('--workers', type=int)
+arguments.add_argument('--disaggregate', action='store_true')
 args = arguments.parse_args()
 
 with Pool(args.workers) as pool:
-    records = pool.imap_unordered(func, each(sys.stdin))
+    records = pool.imap_unordered(func, each(sys.stdin, args))
     pd.concat(records).to_csv(sys.stdout)
