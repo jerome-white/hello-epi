90456d9ae7afc0c88864179fdf85dadf1383e426
Jerome White <jerome@thewex.net>
2020-05-29 07:42:58 +0000

    Clean old results directories

---
+++ fitting/clean.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+from argparse import ArgumentParser
+from multiprocessing import Pool
+
+def func(args):
+    contents = list(args.iterdir())
+    if not any([ x.suffix.endswith('png') for x in contents ]):
+        for i in contents:
+            i.unlink()
+        args.rmdir()
+
+        return args
+
+arguments = ArgumentParser()
+arguments.add_argument('--results', type=Path, required=True)
+arguments.add_argument('--workers', type=int)
+args = arguments.parse_args()
+
+with Pool(args.workers) as pool:
+    for i in filter(None, pool.imap_unordered(func, args.results.iterdir())):
+        print(i)
