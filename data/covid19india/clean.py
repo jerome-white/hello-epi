de7dfd0b98de7de257b47eb114c5085b9ad7bd0b
Jerome White <jerome@thewex.net>
2020-06-01 09:43:55 +0000

    Disable support for disaggretation

--- data/covid19india/clean.py
+++ data/covid19india/clean.py
@@ -8,33 +8,27 @@ import pandas as pd
 Group = cl.namedtuple('Group', ['state', 'district'])
 
 def func(args):
-    (dis, group, df) = args
+    (group, df) = args
 
-    view = (df
+    return (df
             .filter(items=['infected', 'recovered', 'deceased'])
             .resample('D')
-            .ffill())
-    if dis:
-        view = view.diff().dropna()
-    view = (view
+            .ffill()
             .clip(lower=0)
             .astype(int)
             .assign(**group._asdict()))
 
-    return view
-
-def each(fp, args):
+def each(fp):
     index = 'date'
     df = pd.read_csv(sys.stdin, index_col=index, parse_dates=[index])
 
     for (i, g) in df.groupby(list(Group._fields), sort=False):
-        yield (args.disaggregate, Group(*i), g)
+        yield (Group(*i), g)
 
 arguments = ArgumentParser()
 arguments.add_argument('--workers', type=int)
-arguments.add_argument('--disaggregate', action='store_true')
 args = arguments.parse_args()
 
 with Pool(args.workers) as pool:
-    records = pool.imap_unordered(func, each(sys.stdin, args))
+    records = pool.imap_unordered(func, each(sys.stdin))
     pd.concat(records).to_csv(sys.stdout)
