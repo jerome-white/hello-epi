b7d490330e9dc024a2d44fabcd229efbb6eb5e5c
Jerome White <jerome@thewex.net>
2020-06-01 11:52:07 +0000

    Capture warnings as logs

--- fitting/util.py
+++ fitting/util.py
@@ -9,6 +9,7 @@ lvl = os.environ.get('PYTHONLOGLEVEL', 'WARNING').upper()
 fmt = '[ %(asctime)s %(levelname)s %(process)d ] %(message)s'
 logging.basicConfig(format=fmt, datefmt="%d %H:%M:%S", level=lvl)
 logging.getLogger('matplotlib').setLevel(logging.CRITICAL)
+logging.captureWarnings(True)
 Logger = logging.getLogger(__name__)
 
 Split = cl.namedtuple('Split', 'train, test')
