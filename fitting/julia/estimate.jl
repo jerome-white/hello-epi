16e45f84d56fa20bbc1e19007095e78a57337304
Jerome White <jerome@thewex.net>
2020-06-09 22:03:20 +0530

    Refactor model-related functions into their own file

--- fitting/julia/estimate.jl
+++ fitting/julia/estimate.jl
@@ -1,57 +1,12 @@
 using
-    CSV,
     Optim,
     Turing,
-    DataFrames,
-    Distributions,
-    DifferentialEquations
+    # DataFrames,
+    Distributions
+
+include("util.jl")
 
 # disable_logging(Logging.Warn)
-
-function sird(N)
-    return function (du, u, p, t)
-        (S, I, _, _) = u
-        (beta, gamma, mu) = p
-
-        dS = beta * I * S / N
-
-        du[1] = -dS
-        du[3] = gamma * I
-        du[4] = mu * I
-        du[2] = dS - du[3] - du[4]
-    end
-end
-
-function load(fp)
-    compartments = [
-        :susceptible,
-        :infected,
-        :deceased,
-        :recovered,
-    ]
-
-    return select(sort(CSV.read(fp), [:date]), compartments, copycols=false)
-end
-
-function solver(df)
-    steps = nrow(df)
-
-    ode = sird(maximum(sum.(eachrow(df))))
-    u0 = convert(Array, first(df, 1))
-    tspan = (0.0, convert(Float64, steps))
-    prob = ODEProblem(ode, u0, tspan)
-
-    saveat = collect(range(1, stop=steps, length=steps))
-
-    return function (p)
-        s = solve(prob, Tsit5(); saveat=saveat, p=p)
-        if s.retcode != :Success
-            ErrorException(s.retcode)
-        end
-
-        return Matrix(vcat(s.u...))
-    end
-end
 
 function learn(data, observe)
     @model f(x, ::Type{T} = Float64) where {T} = begin
