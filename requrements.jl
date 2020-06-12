cf60254f7cb417812da92d4a2d405099d5c9df8e
Jerome White <jerome@thewex.net>
2020-06-12 13:13:48 +0000

    Package requirements for Julia
    Julia has a proper way of doing this. This is a quick hacky method.

---
+++ requrements.jl
@@ -0,0 +1,14 @@
+import Pkg
+
+Pkg.add([
+    "CSV",
+    "Optim",
+    "Turing",
+    "ArgParse",
+    "StatsPlots",
+    "MCMCChains",
+    "DataFrames",
+    "SharedArrays",
+    "Distributions",
+    "DifferentialEquations",
+])
