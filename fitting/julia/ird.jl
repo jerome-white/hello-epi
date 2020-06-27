62fb1095622a5d0cd7714d0003e647a718e31345
Jerome White <jerome@thewex.net>
2020-06-27 15:24:06 +0000

    Estimate susceptible as fraction of population
    Instead of trying to estimate the susceptibles directly.

--- fitting/julia/ird.jl
+++ fitting/julia/ird.jl
@@ -17,22 +17,26 @@ parameters = [
 
 function EpiModel(N::Number)
     return function (du, u, p, t)
-        (I, _, _) = u
+        (I, R, D) = u
         (beta, gamma, mu, rho) = p
 
-        du[2] = gamma * I
-        du[3] = mu * I
-        du[1] = beta * I * rho / N - du[2] - du[3]
+        S = max(0, N * rho - (I + R + D))
+
+        du[2] = I * gamma
+        du[3] = I * mu
+        du[1] = I * beta * S / N - du[2] - du[3]
     end
 end
 
-function priors(df::DataFrame, N::Number)
-    upper = N - maximum(sum.(eachrow(df)))
+function priors()
     dists = (
+        # truncated(Exponential(1 / 10), 0, 20),
+        # truncated(Exponential(1 / 14), 0, 20),
+        # truncated(Exponential(1 / 600), 0, 1),
         Uniform(0.0, 10.0),
         Uniform(0.0, 5.0),
         Uniform(0.0, 2.0),
-        truncated(Poisson(N), 1, upper),
+        Uniform(0.0, 1.0),
     )
 
     Channel() do channel
