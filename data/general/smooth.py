63ddd032d0015fe79f58a804172984251487f816
Jerome White <jerome@thewex.net>
2020-08-01 12:28:13 +0000

    Name of date column is CLI option

--- data/general/smooth.py
+++ data/general/smooth.py
@@ -5,10 +5,13 @@ import pandas as pd
 
 arguments = ArgumentParser()
 arguments.add_argument('--window', type=int)
+arguments.add_argument('--date-column', default='date')
 args = arguments.parse_args()
 
 df = (pd
-      .read_csv(sys.stdin, index_col='date', parse_dates=['date'])
+      .read_csv(sys.stdin,
+                index_col=args.date_column,
+                parse_dates=[args.date_column])
       .rolling(args.window, center=True)
       .mean()
       .dropna()
